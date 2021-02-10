b27bdcf1c774d62c427dd62dab4833bfee8bca20
Cody <crpeter@iastate.edu>
2021-02-10 16:21:01 -0500

    bond yields and employment urls

---
+++ analysis/jobs_report.py
@@ -0,0 +1,22 @@
+
+
+class jobs_report():
+
+  private_hours_url = 'https://www.bls.gov/news.release/empsit.t18.htm'
+  private_hourly_weekly_earnings_url = 'https://www.bls.gov/news.release/realer.htm'
+  index_weekly_hours_url = 'https://www.bls.gov/news.release/empsit.t20.htm'
+  
+  private_hours_nonSupervisory_url = 'https://www.bls.gov/news.release/empsit.t23.htm'
+  private_hourly_weekly_earnings_nonSupervisory_url = 'https://www.bls.gov/news.release/empsit.t23.htm'
+  index_weekly_hours_nonSupervisory_url = 'https://www.bls.gov/news.release/empsit.t25.htm'
+
+  household_employment_summary_url = 'https://www.bls.gov/news.release/empsit.a.htm'
+  unemployment_reason_rul = 'https://www.bls.gov/news.release/empsit.t11.htm'
+  real_earnings_url = 'https://www.bls.gov/news.release/realer.htm'
+
+  consumer_price_index_url = 'https://www.bls.gov/news.release/cpi.t01.htm'
+
+# unusable need to select table
+  producer_price_index_url = 'https://www.bls.gov/news.release/ppi.toc.htm'
+
+  import_export_price_index_url = 'https://www.bls.gov/news.release/ximpim.nr0.htm'
